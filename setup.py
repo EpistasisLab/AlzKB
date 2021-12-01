11ac054198567a6bd9e8fdca5322a37e495439dd
Joseph D. Romano <jdromano2@gmail.com>
2021-12-01 14:24:11 -0500

    Add basic project structure

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+__all__ = [
+    'VERSION'
+]
+
+import setuptools
+
+MAJOR      = 0
+MINOR      = 1
+MICRO      = 0
+TEST_VER   = 'a'
+ISRELEASED = True
+VERSION    = '%d.%d.%d%s' % (MAJOR, MINOR, MICRO, TEST_VER)
+
+setuptools.setup(
+    name="AlzKB",
+    version=VERSION,
+    author="Joseph D. Romano, Van Truong, Yun Hao, Li Shen, and Jason H. Moore",
+    description="A graph knowledge base for Alzheimer disease",
+    url="https://github.com/EpistasisLab/AlzKB.git",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'build_alzkb=alzkb.build.db:main'
+        ]
+    }
+)
